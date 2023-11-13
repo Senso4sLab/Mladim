eeedc981000ef36c9e5c2f26372aec2e0a552bff
Samo Simončič <samo.simoncic@senso4s.com>
2023-11-13 09:35:01 +0100

    final

--- Mladim.Client/Pages/ActivityResults.razor.cs
+++ Mladim.Client/Pages/ActivityResults.razor.cs
@@ -25,6 +25,8 @@ public partial class ActivityResults
 
     private IEnumerable<SurveyResponsesGroupedByQuestion> SurveyResponsesGroupByQuestions = new List<SurveyResponsesGroupedByQuestion>();
 
+    string resultsRespectTo = "Spol";
+
     protected async override Task OnInitializedAsync()
     {
         if (ActivityId is null)
@@ -74,6 +76,55 @@ public partial class ActivityResults
 
 
 
+    public class CustomStringToBoolConverter : BoolConverter<string>
+    {
 
+        public CustomStringToBoolConverter()
+        {
+            SetFunc = OnSet;
+            GetFunc = OnGet;
+        }
 
+        private string TrueString = "spola";
+        private string FalseString = "starostne skupine";       
+
+        private string OnGet(bool? value)
+        {
+            try
+            {
+                return (value == true) ? TrueString : FalseString;
+            }
+            catch (Exception e)
+            {
+                UpdateGetError("Conversion error: " + e.Message);
+                return TrueString;
+            }
+        }
+
+        private bool? OnSet(string arg)
+        {
+            if (arg == null)
+                return null;
+            try
+            {
+                if (arg == TrueString)
+                    return true;
+                if (arg == FalseString)
+                    return false;
+                else
+                    return null;
+            }
+            catch (FormatException e)
+            {
+                UpdateSetError("Conversion error: " + e.Message);
+                return null;
+            }
+        }
+
+    }
 }
+
+
+
+
+
