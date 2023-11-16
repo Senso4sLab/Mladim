a0de9bf7cfec5d37a755e12e27cfa9ad07fe7f1e
Samo Simončič <samo.simoncic@senso4s.com>
2023-11-16 09:25:28 +0100

    final

--- Mladim.Client/Pages/ActivityResults.razor.cs
+++ Mladim.Client/Pages/ActivityResults.razor.cs
@@ -8,6 +8,7 @@ using System.Collections.Generic;
 using Syncfusion.Blazor.PivotView;
 using MudBlazor;
 using Mladim.Client.Components.Dialogs;
+using Mladim.Client.Utilities.Converters;
 
 namespace Mladim.Client.Pages;
 
@@ -25,7 +26,7 @@ public partial class ActivityResults
 
     private IEnumerable<SurveyResponsesGroupedByQuestion> SurveyResponsesGroupByQuestions = new List<SurveyResponsesGroupedByQuestion>();
     public SurveyResponseSelector selector { get; set; } = new GenderSurveyResponseSelector();   
-
+    private CustomSelectorToBoolConverter customSelectorToBoolConverter = new CustomSelectorToBoolConverter();
     protected async override Task OnInitializedAsync()
     {
         if (ActivityId is null)
@@ -33,14 +34,6 @@ public partial class ActivityResults
 
         SurveyResponsesGroupByQuestions = await GetSurveyResponsesGroupByQuestionAsync();
     }
-
-
-    private void OnSwitcerStateChanged(bool? switchState)
-    {
-        selector = switchState is true ? new AgeGroupSurveyResponseSelector() : new GenderSurveyResponseSelector();       
-    }
-
-
     private async Task<IEnumerable<SurveyResponsesGroupedByQuestion>> GetSurveyResponsesGroupByQuestionAsync()
     {
         IEnumerable<SurveyQuestionVM> surveyQuestions = await SurveyService.GetSurveyQuestionnairyAsync(ActivityId!.Value, Gender.Female);
@@ -54,63 +47,13 @@ public partial class ActivityResults
             .GroupBy(u => u.QuestionResponse.UniqueQuestionId)
             .Select(g => SurveyResponsesGroupedByQuestion.Create(surveyQuestions.FirstOrDefault(sq => sq.UniqueQuestionId == g.Key), g.Select(a => ParticipantQuestionResponse.Create(a.Participant, a.QuestionResponse))))
             .ToList();
-    }
+    }    
 
-    private string RowStyleFunc(SurveyResponsesGroupedByQuestion question, int index)
-    {
-        string rowCss = "font-size: 0.8rem; font-family:poppins; font-weight:400; line-height:1.0; letter-spacing:-0.024rem; color:#6e7191;";
 
-        return index % 2 == 0 ? rowCss + "background-color:white;" : rowCss + "background-color:#EFEFEF;";
-    }
-
-    public class CustomSelectorToBoolConverter : BoolConverter<SurveyResponseSelector>
+    private void OnClickCsvExportFile()
     {
 
-        public CustomSelectorToBoolConverter()
-        {
-            SetFunc = OnSet;
-            GetFunc = OnGet;
-        }
-
-        private SurveyResponseSelector Gender = new GenderSurveyResponseSelector();
-        private SurveyResponseSelector AgeGroup = new AgeGroupSurveyResponseSelector();
-
-        private SurveyResponseSelector OnGet(bool? value)
-        {
-            try
-            {
-                return (value == true) ? Gender : AgeGroup;
-            }
-            catch (Exception e)
-            {
-                UpdateGetError("Conversion error: " + e.Message);
-                return new GenderSurveyResponseSelector();
-            }
-        }
-
-        private bool? OnSet(SurveyResponseSelector? arg)
-        {           
-            try
-            {
-                if (arg == null)
-                    return true;
-                if (arg.GetType() == typeof(GenderSurveyResponseSelector))
-                    return true;
-                if (arg.GetType() == typeof(AgeGroupSurveyResponseSelector))
-                    return false;
-                else
-                    return true;
-            }
-            catch (FormatException e)
-            {
-                UpdateSetError("Conversion error: " + e.Message);
-                return null;
-            }
-        }
-
-    }   
-
-    
+    }
 }
 
 
---
+++ Mladim.Client/Utilities/Converters/CustomSelectorToBoolConverter.cs
@@ -0,0 +1,51 @@
+﻿using Mladim.Client.ViewModels.Survey;
+using MudBlazor;
+
+namespace Mladim.Client.Utilities.Converters;
+
+public class CustomSelectorToBoolConverter : BoolConverter<SurveyResponseSelector>
+{
+
+    public CustomSelectorToBoolConverter()
+    {
+        SetFunc = OnSet;
+        GetFunc = OnGet;
+    }
+
+    private SurveyResponseSelector Gender = new GenderSurveyResponseSelector();
+    private SurveyResponseSelector AgeGroup = new AgeGroupSurveyResponseSelector();
+
+    private SurveyResponseSelector OnGet(bool? value)
+    {
+        try
+        {
+            return (value == true) ? Gender : AgeGroup;
+        }
+        catch (Exception e)
+        {
+            UpdateGetError("Conversion error: " + e.Message);
+            return new GenderSurveyResponseSelector();
+        }
+    }
+
+    private bool? OnSet(SurveyResponseSelector? arg)
+    {
+        try
+        {
+            if (arg == null)
+                return false;
+            if (arg.GetType() == typeof(GenderSurveyResponseSelector))
+                return true;
+            if (arg.GetType() == typeof(AgeGroupSurveyResponseSelector))
+                return false;
+            else
+                return false;
+        }
+        catch (FormatException e)
+        {
+            UpdateSetError("Conversion error: " + e.Message);
+            return null;
+        }
+    }
+
+}
